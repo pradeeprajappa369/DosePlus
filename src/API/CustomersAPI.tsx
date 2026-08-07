2c070d14474b1397ef0dc99f8d62247296e34816
pradeep r <pradeep.r@avantaifintech.com>
2026-08-07 14:30:57 +0530

    Fet(API) ---> Customer API File Created

---
+++ src/API/CustomersAPI.tsx
@@ -0,0 +1,38 @@
+import instance from "../utils/interceptor";
+
+/* ================= CUSTOMERS ================= */
+
+/** Add Customer */
+export async function AddCustomerApi(data) {
+  const responseData = await instance.post(
+    "api/customers/add-customer",
+    data
+  );
+  return responseData;
+}
+
+/** Get All Customers */
+
+export async function GetAllCustomersApi(params) {
+    const responseData = await instance.get(
+      "api/customers/list",
+      { params }
+    );
+    return responseData;
+  }
+
+/** Get Single Customer */
+export async function GetSingleCustomerApi(customerId) {
+  const responseData = await instance.get(
+    `api/customers/view/${customerId}`
+  );
+  return responseData;
+}
+
+/** Customer Stats */
+export async function GetCustomerStatsApi() {
+  const responseData = await instance.get(
+    "api/customers/stats"
+  );
+  return responseData;
+}
